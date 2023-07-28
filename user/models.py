de25f1a34b95070f785f18e2b37de13b8bd691a5
jongsoo-P <jongsooshi@gmail.com>
2023-07-29 03:54:56 +0900

    User model.py setting

--- user/models.py
+++ user/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 
-# Create your models here.
+class UserManager(BaseUserManager):
+
+    def _create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError('User must have an email')
+        email = self.normalize_email(email)
+        user = self.model(
+            email=email,
+            **extra_fields
+        )
+        user.set_password(password)
+        user.save(using=self.db)
+        return user
+
+    def craete_user(self, email, password, **extra_fields):
+        return self._create_user(email, password, **extra_fields)
+    
+    def create_superuser(self, email, password, **extra_fields):
+        return self._create_user(email, password, is_staff=True, is_superuser=True, **extra_fields)
+    
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, max_length=255)
+    name = models.CharField(max_length=50, null=True, blank=True)
+
+    USERNAME_FIELD = 'email'
+    EMAIL_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
